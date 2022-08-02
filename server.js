1fb7d4eeac1f5bba5da93cf4b09cb41ba4f668b0
guillaumec85 <caliezguillaume@live.fr>
2022-08-02 16:42:17 +0200

    init project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5500;
+app.use(express.json());
+
+
+
+app.listen(PORT, (err)=>{
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`Server is running on port ${PORT}`);
+    }
+})
+
+
+
+
+// const midleware1 = (req, res, next) => {
+//     console.log('midleware1');
+//     next();
+// }
+// app.use(midleware1);
+
+// const extracUserMiddleware = (req, res, next) => {
+//     if (req.query.name) {
+//         req.username = req.query.name;
+//         next();
+//     }else{
+//         res.status(400).send('name is required');
+//     } 
+//     }
+// app.get('/user', extracUserMiddleware, (req, res) => {
+//         console.log(`coucou ${req.username}`);
+// });
+
+// app.get("/",(req,res)=>{
+// res.send("Hello World");
+// });
